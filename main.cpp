77a3a2bd246850fe40198f28a7e7c8f2ceff7ea1
Zyssar <cesar.cabrera.pe@gmail.com>
2025-07-03 19:11:27 -0500

    buscanding

--- main.cpp
+++ main.cpp
@@ -21,6 +21,33 @@ private:
 
     Node* root = nullptr;
     size_t size = 0;
+    // -----------recursiones------------
+    Point<T>* recursiveSearch(Node* node, int x, int y) {
+        if (!node) return nullptr;
+        if (node->point.x == x && node->point.y == y) { //si lo encuentra
+            return &node->point;
+        }
+        //cuando el punto se encuentra igual de y va arriba(norte)
+        //cuando el punto se encuentra igual de x va derecha(este)
+        //tomando en cuenta que no ha encontrando el punto exacto aun
+        if (x < node->point.x && y >= node->point.y) { // si el punto actual esta más a la derecha y abajo (ir arriba izquierda)
+            return recursiveSearch(node->nw, x, y);
+        }
+        if (x >= node->point.x && y >= node->point.y) { //si el punto actual esta más a la izquierda y abajo (ir arriba derecha)
+            return recursiveSearch(node->ne, x, y);
+        }
+        if (x < node->point.x && y < node->point.y) { //si el punto actual esta más a la derecha y arriba (ir abajo izquierda)
+            return recursiveSearch(node->sw, x, y);
+        }
+        return recursiveSearch(node->se, x, y); //si ninguno funciona por defecto ira al que falta(abajo derecha)
+    }
+    //espacio O(n); tiempo O(n)
+public:
+    Point<T> search(const int x, const int y) {
+        return recursiveSearch(root, x, y);
+    }
+
+
 };
 
 
